708b4bd86bc25ea53d9724696173800f97463bd2
exceptionalsgl <exceptionalsgl@gmail.com>
2023-08-15 11:08:55 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,12 +4,11 @@
  * print_alphabet-this is a funtion that print alphabet in lowercase
  * Return: 0
 */
-
 void print_alphabet(void)
 {
 	char i;
-	for (i = 'a' ; i <= 'z' ; i++)
-		_putchar(i);
-	_putchar ('\n');
 
+	for (i = 'a' ; i <= 'z' ; i++)
+	_putchar(i);
+	_putchar ('\n');
 }
